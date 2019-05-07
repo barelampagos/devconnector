3d719623d84b8d2ebbabcdfdde8c706bf0321b60
barelampagos <barelampagos@gmail.com>
2019-05-06 21:26:42 -0700

    Adding profile routes

--- config/db.js
+++ config/db.js
@@ -4,18 +4,19 @@ const db = config.get('mongoURI');
 
 // Async await --> try catch
 const connectDB = async () => {
-  try {
-    await mongoose.connect(db, {
-      useNewUrlParser: true,
-      useCreateIndex: true
-    });
-    console.log('MongoDB Connected');
-  } catch (err) {
-    console.log(err.message);
+	try {
+		await mongoose.connect(db, {
+			useNewUrlParser: true,
+			useCreateIndex: true,
+			useFindAndModify: false
+		});
+		console.log('MongoDB Connected');
+	} catch (err) {
+		console.log(err.message);
 
-    // Exit process w/ failure
-    process.exit(1);
-  }
+		// Exit process w/ failure
+		process.exit(1);
+	}
 };
 
 module.exports = connectDB;
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -23,7 +23,7 @@ router.get('/me', auth, async (req, res) => {
 		}
 	} catch (err) {
 		console.error(err.message);
-		res.status(500).send('Server Error');
+		return res.status(500).send('Server Error');
 	}
 });
 
@@ -114,5 +114,63 @@ router.post(
 		res.send('Hello');
 	}
 );
+
+// @route   GET api/profile/
+// @desc    Get all profiles
+// @access  Public
+router.get('/', async (req, res) => {
+	try {
+		const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+		res.json(profiles);
+	} catch (err) {
+		console.error(err.message);
+		return res.status(500).send('Server Error');
+	}
+});
+
+// @route   GET api/profile/user/:user_id
+// @desc    Get profile by user ID
+// @access  Public
+router.get('/user/:user_id', async (req, res) => {
+	try {
+		const profile = await Profile.findOne({
+			user: req.params.user_id
+		}).populate('user', ['name', 'avatar']);
+
+		if (!profile) {
+			return res.status(400).json({ msg: 'Profile not found.' });
+		}
+
+		res.json(profile);
+	} catch (err) {
+		console.error(err.message);
+
+		if (err.kind == 'ObjectId') {
+			return res.status(400).json({ msg: 'Profile not found.' });
+		}
+
+		return res.status(500).send('Server Error');
+	}
+});
+
+// @route   DELETE api/profile/
+// @desc    Delete profile, user & posts
+// @access  Private
+router.delete('/', auth, async (req, res) => {
+	try {
+		// TODO: Remove users posts
+
+		// Remove profile
+		await Profile.findOneAndRemove({ user: req.user.id });
+
+		// Remove user
+		await User.findOneAndRemove({ _id: req.user.id });
+
+		res.json({ msg: 'User deleted' });
+	} catch (err) {
+		console.error(err.message);
+		return res.status(500).send('Server Error');
+	}
+});
 
 module.exports = router;
